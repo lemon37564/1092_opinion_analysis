e53c7c42d3324d3266a3e7911da9046f81c503fe
elite3312 <44432364+elite3312@users.noreply.github.com>
2021-03-23 10:01:32 +0800

    Add files via upload

---
+++ intermediate_to_detecttion.py
@@ -0,0 +1,28 @@
+import csv
+
+for i in range(1 ,9):
+    try:
+        f = open("dia"+str(i)+"intermediate.tsv", "r",encoding="utf-8")
+        read_tsv = csv. reader(f, delimiter="\t")
+        with open("dia"+str(i)+'detection_train.tsv', 'wt',encoding="utf-8") as out_file:
+            tsv_writer = csv.writer(out_file,delimiter='\t', lineterminator='\n' )
+            is_first_row=1
+            for row in read_tsv:
+                if is_first_row==1:
+                    is_first_row=0
+                    continue
+                if row[0]=='0':
+                    tsv_writer.writerow(['['+"other"+']', row[5]])
+                elif row[0]=='1':
+                    tsv_writer.writerow(['['+"time"+']', row[5]])
+                else:
+                    tsv_writer.writerow(['['+"location"+']', row[5]])
+
+                
+                    
+        f.close()
+    except IOError:
+        print("File"+str(i)+" not accessible")
+
+    
+    
